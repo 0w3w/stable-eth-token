47b0304cb15ed5bbc7ca6e346990eaf5e05fc327
0w3w <0w3w4u@gmail.com>
2018-05-15 00:27:34 -0500

    Add Rinkeby Support

--- truffle-config.js
+++ truffle-config.js
@@ -10,6 +10,13 @@ module.exports = {
       port: 7545,
       network_id: "*", // Match any network id
       gas: "6712390"
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0x968b872E593415e35893Aa54E5344fD6a316ABCf", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
     }
   }
 };
