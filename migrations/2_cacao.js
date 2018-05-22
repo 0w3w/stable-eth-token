0afb2db22e52ae3289a3b5df2dc973dc68780675
0w3w <0w3w4u@gmail.com>
2018-05-22 01:36:21 -0500

    Deployed to Rinkeby

--- migrations/2_cacao.js
+++ migrations/2_cacao.js
@@ -1,6 +1,14 @@
 var Cacao = artifacts.require("Cacao");
-module.exports = function(deployer, network, accounts) {
+var CacaoRescueMock = artifacts.require("test/CacaoRescueMock.sol");
+
+module.exports = function (deployer, network, accounts) {
     deployer.deploy(Cacao,
         accounts[1], accounts[2], accounts[3], accounts[4], // Creation Addresses (Including msg.sender as #1)
-        accounts[5], accounts[6], accounts[7]); // Distribution Addresses
+        accounts[5], accounts[6], accounts[7],
+        { gas: 7499999, from: accounts[0] }); // Distribution Addresses
+    deployer.deploy(CacaoRescueMock,
+        accounts[1], accounts[2], accounts[3], accounts[4], // Creation Addresses (Including msg.sender as #1)
+        accounts[5], accounts[6], accounts[7], // Distribution Addresses
+        1, // Current Block Number
+        { gas: 7499999, from: accounts[0] });
 };
--- migrations/3_CacaoRescueMock.js
+++
@@ -1,7 +0,0 @@
-var CacaoRescueMock = artifacts.require("test/CacaoRescueMock.sol");
-module.exports = function(deployer, network, accounts) {
-    deployer.deploy(CacaoRescueMock,
-        accounts[1], accounts[2], accounts[3], accounts[4], // Creation Addresses (Including msg.sender as #1)
-        accounts[5], accounts[6], accounts[7], // Distribution Addresses
-        1); // Current Block Number
-};
